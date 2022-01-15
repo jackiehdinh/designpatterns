970a2339a760ede91a2d6e5c7801a0eb7c7e7ab6
Kim Dinh <dinh@Kims-MacBook-Air.local>
2022-01-15 12:08:30 -0500

    Added Hewwo

---
+++ strategy/Hewwo.java
@@ -0,0 +1,11 @@
+public class Hewwo {
+    
+/**
+ * A Video Game Character 
+ * @author Kim Dinh
+ */
+
+    public static void main(String[] args) {
+        System.out.println("Hewwo uww");
+    }
+}
