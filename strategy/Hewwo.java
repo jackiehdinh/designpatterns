fd3247f965892f1ac360e727c062d970a70c9184
Kim Dinh <dinh@Kims-MacBook-Air.local>
2022-01-15 12:12:36 -0500

    Fixed Hewwo

--- strategy/Hewwo.java
+++ strategy/Hewwo.java
@@ -1,10 +1,5 @@
 public class Hewwo {
     
-/**
- * A Video Game Character 
- * @author Kim Dinh
- */
-
     public static void main(String[] args) {
         System.out.println("Hewwo uww");
     }
